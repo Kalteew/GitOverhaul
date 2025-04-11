489834e38bc1e09dc209061b5b95a7afb83c52cd
GPTDev <gptdev@local>
2025-04-11 16:25:47 +0000

    refactor: switch to classic controller setup in Program.cs

--- GitOverhaul/GitOverhaul.Api/Program.cs
+++ GitOverhaul/GitOverhaul.Api/Program.cs
@@ -1,33 +1,28 @@
-using GitOverhaul.Api.Features.Git;
 using GitOverhaul.Api.Features.OpenAi;
 using GitOverhaul.Api.Middleware;
 using GitOverhaul.Domain.Services;
 using GitOverhaul.Infra.Services;
+using Swashbuckle.AspNetCore.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IGitService, GitService>();
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Construction de l'application (runtime pipeline, endpoints, DI, etc)
 var app = builder.Build();
 
 app.UseStaticFiles();
-app.UseSwagger(c => { c.RouteTemplate = "swagger/{documentName}/swagger.json"; });
+app.UseSwagger(c =>
+{
+    c.RouteTemplate = "swagger/{documentName}/swagger.json";
+});
 app.UseMiddleware<ErrorMiddleware>();
 app.UseSwaggerUI();
 
-var gitGroup = app.MapGroup("/git");
-gitGroup.MapGetStructure()
-    .MapReadFile()
-    .MapPushChanges()
-    .MapCreateBranch();
+app.MapControllers();
 
-var openaiGroup = app.MapGroup("/openai");
-openaiGroup.MapOpenAiSchemaGenerator();
-
-// Déclenche une requête HTTP locale vers /openai/schema après 10s en dev
 // app.TriggerDelayedSchemaGeneration();
 
 app.Run();
